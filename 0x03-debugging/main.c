a8442b13143aa5869c1161b3e2dc5305200759e1
GarismoHussien <hand2land@gmail.com>
2023-06-23 13:21:09 -0700

    Ttttttttttt

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,28 +1,24 @@
-#include "main.h"
-<<<<<<< HEAD
+#include"main.h"
+<<<<<<<HEAD
 =======
-
->>>>>>> c2815419bc600fd48eefad837c71a4bbde7df5ad
+>>>>>>>c2815419bc600fd48eefad837c71a4bbde7df5ad
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
+*main-testsfunctionthatprintsifintegerispositiveornegative
+*Return:0
 */
-<<<<<<< HEAD
-int main(void)
+<<<<<<<HEAD
+intmain(void)
 {
-int i;
-i = 98;
+inti;
+i=98;
 positive_or_negative(i);
-return (0);
+return(0);
 =======
-
-int main(void)
+intmain(void)
 {
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
->>>>>>> c2815419bc600fd48eefad837c71a4bbde7df5ad
+inti;
+i=98;
+positive_or_negative(i);
+return(0);
+>>>>>>>c2815419bc600fd48eefad837c71a4bbde7df5ad
 }
