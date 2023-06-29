dbcd4e0c1e410622d0773488633c4ae6d8435ab8
GarismoHussien <hand2land@gmail.com>
2023-06-29 14:43:23 -0700

    T

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,28 +1,28 @@
-#include"main.h"
+#include "main.h"
 /**
-*_strncpy-copyastring
-*@dest:inputvalue
-*@src:inputvalue
-*@n:inputvalue
-*
-*Return:dest
-*/
-char*_strncpy(char*dest,char*src,intn)
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
 {
-intj;
+	int j;
 
-j=0;
-while(j<n&&src[j]!='\0')
-{
-dest[j]=src[j];
-j++;
-}
-while(j<n)
-{
-dest[j]='\0';
-j++;
-}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 
-return(dest);
+	return (dest);
 }
 
