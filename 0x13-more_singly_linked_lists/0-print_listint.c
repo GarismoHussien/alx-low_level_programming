0c999ddd21c4d645e48b476bf0f02b26d78cf8f2
GarismoHussien <hand2land@gmail.com>
2023-08-01 22:37:09 +0000

    Test descr

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,14 +1,11 @@
 #include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
  *
- * Description: singly linked list node structure
- **
- *
-*/
+ * Return: number of nodes
+ */
 size_t print_listint(const listint_t *h)
 {
 size_t num = 0;
