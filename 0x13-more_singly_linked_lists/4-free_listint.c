3ab03507f5c0263e1b99acdd695079c3ab94fa96
GarismoHussien <hand2land@gmail.com>
2023-08-01 22:46:15 +0000

    test new files

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+nclude "lists.h"
+
+/**
+* free_listint - frees a linked
+* @head: listint_t list to be free
+*/
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
+}
