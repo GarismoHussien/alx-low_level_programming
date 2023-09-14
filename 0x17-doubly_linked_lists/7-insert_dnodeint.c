6f33e1d0afbe760097edc930a96afd60d442da04
GarismoHussien <hand2land@gmail.com>
2023-09-14 16:55:10 -0700

    Cmp

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,6 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new;
 	dlistint_t *head;
 	unsigned int i;
+
 	new = NULL;
 	if (idx == 0)
 		new = add_dnodeint(h, n);
