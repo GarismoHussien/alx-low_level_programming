289fd468ec9d8d06e6d6a0e75d61cffad49e211d
GarismoHussien <Hand2land@gmail.com>
2023-10-06 17:03:57 -0700

    addREADME

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,16 +2,18 @@
 
 /**
  * hash_djb2 - Hash function implementing the djb2 algorithm.
- * @str: The string to hash.
+ * @str:  string to hash.
  *
- * Return: The calculated hash.
+ * Return:  calculated hash.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-unsigned long int hash;
-int c;
-hash = 5381;
-while ((c = *str++))
-hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-return (hash);
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return (hash);
 }
